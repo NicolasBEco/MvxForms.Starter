0959acee9606abea24a476075d08a6dd40f072bc
Nicolas BARBE <nicolas.barbe@econocom.com>
2017-10-31 14:57:42 +0100

    add other test projects

---
+++ MvxForms.Starter.UnitTests.ViewModels/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MvxForms.Starter.UnitTests.ViewModels
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
